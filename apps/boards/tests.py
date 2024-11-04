be8c0490e94f1b04bae5eda7e8fbffea3678010c
Tobias Kim <madeffort0903@gmail.com>
2024-11-05 02:30:08 +0900

    users 앱 테스트 코드 작성 중...

--- apps/boards/tests.py
+++ apps/boards/tests.py
@@ -1,15 +1,1 @@
 from django.test import TestCase
-
-# from ninja.testing import TestClient
-# from config.api import api
-
-
-# # Create your tests here.
-# class BoardTest(TestCase):
-
-#     def setUp(self):
-#         self.client = TestClient(api)
-
-#     def test_addition(self):
-#         """간단한 덧셈 테스트"""
-#         assert 1 + 1 == 2
--- apps/users/tests.py
+++ apps/users/tests.py
@@ -1,3 +1,35 @@
 from django.test import TestCase
+from ninja.testing import TestClient
+from config.api import api
 
-# Create your tests here.
+from apps.users.models import User
+from apps.users.schemas import UserSchema
+
+
+class UserControllerTest(TestCase):
+
+    @classmethod
+    def setUpClass(cls):
+        super().setUpClass()
+        cls.client = TestClient(api)
+
+    def setUp(self):
+        self.user1 = User.objects.create_user(
+            username="user1@example.com", password="password123"
+        )
+        self.user2 = User.objects.create_user(
+            username="user2@example.com", password="password123"
+        )
+
+    def test_get_users(self):
+        """유저 전체 조회"""
+        response = self.client.get("/api/users/")
+
+        # 예상 데이터
+        expected_data = [
+            UserSchema.from_orm(self.user1).dict(),
+            UserSchema.from_orm(self.user2).dict(),
+        ]
+
+        assert response.status_code == 200
+        assert response.json() == expected_data
