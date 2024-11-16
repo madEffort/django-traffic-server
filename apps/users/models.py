6190acfa53642dd1a936fcb5f1e80f3b3896b954
Tobias Kim <madeffort0903@gmail.com>
2024-11-16 21:09:47 +0900

    User 모델에 다중기기 속성 추가(ArrayField)

--- apps/users/models.py
+++ apps/users/models.py
@@ -1,10 +1,16 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
+from django.contrib.postgres.fields import ArrayField
 
 
 class User(AbstractUser):
 
     username = models.EmailField(unique=True)
+    device = ArrayField(
+        models.CharField(max_length=255),
+        blank=True,
+        default=list,
+    )
 
     USERNAME_FIELD = "username"
     REQUIRED_FIELDS = []
