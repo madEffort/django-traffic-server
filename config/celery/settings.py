be6c3333c23ce4fb1b108b81f400d35a5f478d38
Tobias Kim <madeffort0903@gmail.com>
2024-11-17 17:04:29 +0900

    celery 브로커 rabbitmq로 변경

--- config/celery/settings.py
+++ config/celery/settings.py
@@ -1,9 +1,17 @@
 from celery.schedules import crontab
 from decouple import config
 
-CELERY_BROKER_URL = f'redis://:{config("REDIS_PASSWORD")}@redis:6379/0'
+# CELERY_BROKER_URL = f'redis://:{config("REDIS_PASSWORD")}@redis:6379/0'
+CELERY_BROKER_URL = (
+    f'amqp://{config("RABBITMQ_USER")}:{config("RABBITMQ_PASSWORD")}@rabbitmq:5672/'
+)
 
+CELERY_RESULT_BACKEND = f'redis://:{config("REDIS_PASSWORD")}@redis:6379/0'
+CELERY_ACCEPT_CONTENT = ["json"]
+CELERY_TASK_SERIALIZER = "json"
+CELERY_RESULT_EXPIRES = 3600  # 결과 데이터 유효 기간 (1시간)
 
+# beat
 CELERY_BEAT_SCHEDULE = {
     "aggregate_and_insert_campaigns_clicks": {
         "task": "apps.campaigns.tasks.aggregate_and_insert_campaigns_clicks",
